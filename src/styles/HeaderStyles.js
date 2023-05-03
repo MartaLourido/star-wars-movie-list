de4c46d741489e171d35cf8d7a4ab7ab9cb43522
MartaLourido <martalouri@gmail.com>
2023-05-03 14:21:06 +0200

    updated basic layout styles in headerStyles and AppStyles

--- src/styles/HeaderStyles.js
+++ src/styles/HeaderStyles.js
@@ -1,19 +1,22 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
+  align-items: center;
   background-color: #f0f0f0;
   display: flex;
   justify-content: space-between;
   padding: 1rem;
 `;
 
-export const SortButton = styled.button`
-`;
-
 export const SearchContainer = styled.div`
   display: flex;
   flex-grow: 1;
   margin-left: 1rem;
+  width: 100%;
+`;
+
+export const SortButton = styled.button`
+  width: 20%
 `;
 
 export const SearchInput = styled.input`
